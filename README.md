0c2099eb2534a0cf76889a00969732669318714a
Abhijit Roy <dalvath3700@gmail.com>
2019-08-12 12:03:54 +0530

    Tasks completed
    - Download mail attachments by subject - 'test'
    - fetch downloaded excel data into df1
    - fetch output excel data into df2
    - TODO: * fetch required cell data from df1 to df2, * prepare documment the order form

---
+++ README.md
@@ -0,0 +1,13 @@
+# Work-8
+Update the form from datasets attached in Outlook email
+
+## Installation
+* Download and Install [Anaconda](https://www.anaconda.com/distribution/#download-section)
+> NOTE: Don't forget to tick the checkbox corresponding to "Add to path". This will enable using `conda` in the terminal.
+* Open the terminal and check for following packages
+```
+pandas
+numpy
+```
+* If not found, then run these 2 commands in terminal:
+	- `pip install pandas`
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import win32com.client
 import os
-import xlwings
+import pandas as pd
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case the inbox. You can change that number to reference
@@ -9,7 +9,7 @@ message = messages.GetFirst()
 subject = message.Subject
 
 # path of directory where attached files are to be saved locally
-get_path = 'C:\\Users\\abhijit\\Desktop\\mail'
+get_path = 'I:\\github_repos\\work-8\\download_mail'
 # print(len(messages))      # length of the messages in "Inbox" folder
 
 # Loop in all the Inbox messages
@@ -27,3 +27,19 @@ for m in messages:
 
     else:
         message = messages.GetNext()
+
+df_attachments = []
+for dirname, dirpath, files in os.walk("./download_mail/"):
+    for file in files:
+        # print(file)
+        df_attachments.append(pd.ExcelFile("./download_mail/" + file).parse(0))      # parse sheet_name - 0 i.e. 1st sheet
+
+# define `df1` i.e. from downloaded mail attachments (in excel format)
+df1 = df_attachments[0]
+# print(df1.head())
+
+# define `df2` i.e. output
+df2 = pd.ExcelFile("./output/Attachment_1564702282.xlsx").parse(0)              # parse sheet_name - 0 i.e. 1st sheet
+# print(df2)
+
+
