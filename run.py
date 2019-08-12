cd357402af22cdab3b86dbe4ec5187b98be515e7
Abhijit Roy <dalvath3700@gmail.com>
2019-08-12 13:15:27 +0530

    Filtering done in codebase

--- run.py
+++ run.py
@@ -42,4 +42,46 @@ df1 = df_attachments[0]
 df2 = pd.ExcelFile("./output/Attachment_1564702282.xlsx").parse(0)              # parse sheet_name - 0 i.e. 1st sheet
 # print(df2)
 
+"""
+Map 2 column names: df1 --> df2
+===============================
+df1                     df2
+-----------------------------------------
+"Order ID"              "ClientOrderCode"  
+"Billing First Name"    "FirstName"
+"Shipping Address 1"    "Address1"
+"Shipping City"         "City"
+"Shipping State"        "Province"
+"Shipping Postcode"     "PostCode"
+"Shipping Country"      "Country"
+"SKU"                   "OrderSourceSKU"
+"Purchased"             "ProductNum"
+"""
+col_1 = ["Order ID", "Billing First Name", "Shipping Address 1", "Shipping City",  "Shipping State", 
+        "Shipping Postcode", "Shipping Country", "SKU", "Purchased"]
+col_2 = ["ClientOrderCode", "FirstName", "Address1", "City", "Province", "PostCode", "Country", "OrderSourceSKU", "ProductNum"]
+
+# define `df1_col1` for `df1` as per the required columns i.e. col_1
+df1_col1 = df1[col_1]
+# print(df1_col1.head())
+
+# define `df2_col2` for `df2` as per the required columns i.e. col_2
+df2_col2 = df1_col1             # copy the content from `df1_col1` into `df2_col2`
+df2_col2.columns = col_2        # add columns
+# print(df2_col2.head())
+
+# drop all rows of `df2`
+df2.drop(df2.index, inplace=True)
+
+"""
+NOTE: Before applying this, `df2` needs to be cleared first.
+and then copy respective columns data into from `df2_col2` into `df2`
+"""
+for c in col_2:
+    df2[c] = df2_col2[c]
+
+# print(df2)
+
+# print the `df2` (w/o index) into already present excel file 
+df2.to_excel("./output/Attachment_1564702282.xlsx", index= False)
 
